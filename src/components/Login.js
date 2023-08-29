fc8dd10ab9a2cbbdd3d5ab7ecdf4bca047345274
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-08-30 01:43:32 +0530

    CSS content added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,32 @@
 import styled from 'styled-components'
 
 const Login = (props) => {
-    return <div>Login</div>;
+    return(
+        <Container>
+            <Content>Content</Content>
+        </Container>
+    )
 };
+
+const Container = styled.section`
+overflow: hidden;
+display: flex;
+flex-direction: column;
+text-align: center;
+height: 100vh;
+`;
+const Content = styled.div`
+margin-button: 10vw;
+width: 100%;
+position: relative;
+min-height: 100vh;
+box-sizing: border-box;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+padding: 60px 40px;
+height: 100%;
+`;
 
 export default Login;
