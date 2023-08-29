d74c3c0329b867e99c4f01ce5d502ebbd790a95c
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-08-29 23:53:12 +0530

    App.js File Updated with dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Login from './components/Login';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello How are you people</h2>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Login/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
