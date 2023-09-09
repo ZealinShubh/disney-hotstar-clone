d6d8753a7b0ebb13b885841fb8a2b9578f6ed74a
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-09-09 12:24:01 +0530

    SignOut functionality added

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import {
     selectUserName,
     selectUserPhoto,
     setUserLoginDetails,
+    setSignOutState,
 } from "../features/users/userSlice";
 import { useEffect } from "react";
 
@@ -26,12 +27,19 @@ const Header = (props) =>{
     
 
     const handleAuth = () =>{
+        if (!userName){
         auth.signInWithPopup(provider).then((result) =>{
             setUser(result.user);
             // navigate('/home');
         }).catch((error) => {
             alert(error.message);
         });
+    } else if(userName){
+        auth.signOut().then(() => {
+            dispatch(setSignOutState())
+            navigate('/')
+        }).catch((err) => alert(err.message));
+    }
     };
 
     const setUser = (user) => {
@@ -81,7 +89,12 @@ const Header = (props) =>{
             <span>SERIES</span>
            </a>
         </NavMenu>
-        <UserImg src={userPhoto} alt={userName} />
+        <SignOut>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
+                <span onClick={handleAuth}>Sign Out</span>
+            </DropDown>
+        </SignOut>
         </>
         )}
     </Nav>
@@ -202,6 +215,45 @@ cursor: pointer;
 
 const UserImg = styled.img`
 height: 100%;
+`;
+
+const DropDown = styled.div`
+position: absolute;
+top: 48px;
+right: 0px;
+background: rgb(19, 19, 19);
+border: 1px solid rgba(151, 151, 151, 0.34);
+border-radius: 4px;
+box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+padding: 10px;
+font-size: 14px;
+letter-spacing: 3px;
+width: 101px;
+opacity: 0;
+
+`;
+
+const SignOut = styled.div`
+position: relative;
+height: 48px;
+width: 48px;
+display: flex;
+cursor: pointer;
+align-items: center;
+justify-content: center;
+
+${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+}
+
+&:hover {
+    ${DropDown}{
+        opacity: 1;
+        transition-duration: 1s;
+    }
+}
 `;
 
 export default Header;
