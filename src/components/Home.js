e0763376c2463fe70ec55d21b1c5da67ede23da0
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-09-11 00:06:41 +0530

    Added Trending Component to the page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Viewers from './Viewers';
 import Recommends from './Recommends';
 import NewDisney from './NewDisney';
 import Originals from './Originals';
+import Trending from './Trending';
 
 const Home = (props) =>{
     return (
@@ -13,6 +14,7 @@ const Home = (props) =>{
             <Recommends />
             <NewDisney />
             <Originals />
+            <Trending />
         </Container>
     );
 };
