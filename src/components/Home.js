6d5947077a4d0677ce55a12a5879085ed8124286
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-09-11 01:59:06 +0530

    Added slight changes in comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ const Home = (props) =>{
     useEffect(() => {
         db.collection('movies').onSnapshot((snapshot) => {
             snapshot.docs.map((doc) => {
-                console.log(recommends);
+                // console.log(recommends);
                 switch(doc.data().type) {
                     case "recommend":
                         recommends = [...recommends, { id: doc.id, ...doc.data() }];
