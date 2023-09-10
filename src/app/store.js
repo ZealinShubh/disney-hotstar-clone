777a065196de4f3085b6f8f20c2402a6b69e8708
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2023-09-11 01:55:08 +0530

    Added data and fetched it from firestore

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import useReducer from "../features/users/userSlice";
+import movieReducer from "../features/movie/movieSlice";
 
 export default configureStore({
     reducer: {
         user: useReducer,
+        movie: movieReducer,
     },
     middleware: getDefaultMiddleware({
         serializableCheck: false,
